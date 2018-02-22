d66ccb11f8d15ab60a07d85971029d932e783630
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-02-22 00:27:25 -0300

    Alteração no gulpfile, adicionado scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/css/*.css").on('change', browserSync.reload);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
