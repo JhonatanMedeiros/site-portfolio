729b257688c5e3c25c00f888e30e56a9fa2af9c4
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-02-24 02:44:52 -0300

    Alteração de script do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var del = require('del');
+
+
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'js-watch'], function() {
 
     browserSync.init({
         server: "./app"
@@ -18,14 +29,17 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(concat('main.css'))
+        .pipe(minifyCSS())
         .pipe(gulp.dest("app/css"))
+        .pipe(notify({ message: 'Styles task complete' }))
         .pipe(browserSync.stream());
 });
 
 // process JS files and return the stream.
 gulp.task('js', function() {
     return gulp.src('app/js/*js')
-        .pipe(browserify())
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
@@ -36,5 +50,54 @@ gulp.task('js-watch', ['js'], function(done) {
     browserSync.reload();
     done();
 });
+
+
+gulp.task('images', function() {
+    return gulp.src('app/assets/img/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/assets/img'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+
+
+gulp.task('dist', ['clean'], function (done) {
+    gulp.start('dist-sass', 'dist-js', 'images', 'dist-html');
+
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('dist-sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"));
+
+
+});
+
+gulp.task('dist-js', function() {
+    return gulp.src('app/js/*js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('dist-html', function () {
+    return gulp.src('app/index.html')
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify({ message: 'Dist Html task complete' }));
+});
+
+gulp.task('clean', function() {
+    return del(['dist/','dist/css', 'dist/js', 'dist/assets/img']);
+});
+
+
+
+
+
+
 
 gulp.task('default', ['serve']);
